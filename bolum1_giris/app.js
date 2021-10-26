775efda0fc6e37a9f1919e7dac161fc5e84dab30
mucahit2664 <mucahit2664@gmail.com>
2021-10-27 00:02:32 +0300

    da02

--- bolum1_giris/app.js
+++ bolum1_giris/app.js
@@ -5,9 +5,9 @@
 console.log("Hello JavaScript");
 console.log("Javascript javadan daha kolay bir dildir..");
 console.error("bu bir hatadir");
-console.warn("bu bir hatadir");
+console.warn("bu bir uyaridir");
 
-alert("Dikkat et Mucahit abi..");// ? Ekrana pop-up mesaj çıkarır.
+//alert("Dikkat et Mucahit abi..");// ? Ekrana pop-up mesaj çıkarır.
 
 // * =======================================================
 // *                 DEĞİŞKEN TANIMLAMA
@@ -17,3 +17,64 @@ alert("Dikkat et Mucahit abi..");// ? Ekrana pop-up mesaj çıkarır.
 // ? Ancak, modern JS var keyword'unun kullanımı azaldı.
 //? Çünkü, var global değişken gibi düşünülebilir.
 // ? Günümüzde programcılar global değişken gerekmedikçe VAR tercih etmemektedir.
+
+var pi=3.14;
+console.log(3.14);
+console.log(typeof pi)//number
+
+pi = "3";
+console.log(pi);
+console.log(typeof pi)//string
+
+
+pi = true;
+console.log(pi);
+console.log(typeof pi);//Boolean
+
+
+isim = "Mucahit";
+console.log(isim);
+console.log(typeof isim);//string
+
+
+var gel;
+console.log(gel);
+console.log(typeof gel)//undefined
+
+//************************ CONST ****************************
+// ? const ve let Modern JS ile gelmiş yeni değişken tanımlama keyword'leridir.
+// ! CONST: Sadece başlangıçta değer atılabilir.
+// ! Sonradan değeri değiştirilemez (non-primitive'ler hariç).
+
+const piSayisi=3.14;
+console.log(piSayisi);
+//piSayisi=3;//deger desgistirilemez
+//************************ LET ****************************
+// ? Eğerki değişkenimİzin değerinin değişmesi gerekiyor ise const kullanamayız.
+// ? Bunun yerine LET kullanabiliriz.
+// ! LET Block-scope bir değişkendir. VAR ise global-scope bir tanımlamadır.
+// * Block-scope: Sadece tanımlı olduğu alanda geçerlidir.
+let fiyat;
+console.log(fiyat);
+fiyat=5.5;
+console.log(fiyat);
+console.log(typeof fiyat);
+//TODO String tanımlamak için 3 farklı karakter kullanılabilir.
+let isim1 = "mucahit";
+console.log(isim1)
+let isim2 = 'refik';
+console.log(isim2);
+let isim3 = `sefik`;
+console.log(isim3);
+
+const dil = null;
+console.log(dil);
+console.log(typeof dil);//object
+// * =======================================================
+// *                 ARİTMETİK OPERATÖRLER
+// * =======================================================
+
+
+
+
+
