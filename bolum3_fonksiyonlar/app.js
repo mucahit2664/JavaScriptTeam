e0b0853b79b9b46cde802e64bfd424e87d3a9f3e
mucahit2664 <mucahit2664@gmail.com>
2021-11-04 18:46:30 +0300

    DAY4

---
+++ bolum3_fonksiyonlar/app.js
@@ -0,0 +1,14 @@
+// * =======================================================
+// *                    FONKSİYONLAR
+// * =======================================================
+// ! 1.YÖNTEM  : FUNCTION DECLARATION
+// Fonksiyonun tanımlanması
+function yazdiralim() {
+    console.log("Seyrani bey");
+  }
+  yazdiralim(); //! call \ invoke
+  // Ornek: Parametreli Fonksiyon
+  function yazdiralim2(ad, soyad) {
+    console.log(`Adiniz ${ad} soyadiniz ${soyad} dir`);
+  }
+  yazdiralim2("Refik", "Gulak");
