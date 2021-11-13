5945ed80b6319367fbe40af9c0e4f6e8fbe68743
mucahit2664 <mucahit2664@gmail.com>
2021-11-13 17:56:08 +0300

    day30

--- bolum3_fonksiyonlar/app.js
+++ bolum3_fonksiyonlar/app.js
@@ -31,17 +31,80 @@
 //   return sonuc;
 // }
 // ! 2.YÖNTEM  : FUNCTION EXPRESSION
-const tekCift1 = function (sayiniz) {
-  6;
-  return sayiniz % 2 == 0 ? "CIFT" : "TEK";
-};
-console.log(tekCift1(42));
+//const tekCift1 = function (sayiniz) {
+//  6;
+//  return sayiniz % 2 == 0 ? "CIFT" : "TEK";
+//};
+//console.log(tekCift1(42));
 //ORNEK: Buyuk sayiyi bulma
+
 // ! 3.YÖNTEM  : ARROW (OK) FONKSIYONLARI
-const sayi1 = Number(prompt("1.sayiyi giriniz:"));
-const sayi2 = Number(prompt("2.sayiyi giriniz:"));
-const topla = (sayi1, sayi2) => sayi1 + sayi2;
-alert(topla(sayi1, sayi2));
-console.log(topla(sayi1, sayi2));
-const topla2 = (a, b) => a + b;
-console.log(topla2(sayi1, sayi2));
+
+//const sayi1 = Number(prompt("1.sayiyi giriniz:"));
+//const sayi2 = Number(prompt("2.sayiyi giriniz:"));
+
+//const topla = (sayi1, sayi2) => sayi1 + sayi2;
+
+//alert(topla(sayi1, sayi2));
+//console.log(topla(sayi1, sayi2));
+
+//const topla2 = (a, b) => a + b;
+//console.log(topla2(sayi1, sayi2));
+
+//Ornek :
+
+const tekMi2 = (sayi) => (sayi % 2 ==0 ? "cift":"tek" );
+console.log(tekMi2(5));//call - invoke
+console.log(tekMi2(6)); 
+
+//*Ornek :Us Alma
+
+//const taban=prompt("taban giriniz");
+//const us =prompt("us giriniz");
+
+//const usAl=(k, l) => k **l ;
+//!bizim kullanicidan aldigimiz degerleri fonksiyon ismine bakmadan
+//!sadece degerleri kullaniyor(parametrelerin-argumentlerin)
+
+//console.log(usAl(taban, us));//invoke
+
+// * Ornek: Menü
+// ! Ok fonksiyonunda birden fazla ifade(EXPERESSION) var ise fonksiyonda suslu parantez kullanmalıyız.
+
+const menu =()=>{
+
+console.log("===============================================");
+console.log("             JAVASCRIPT                        ");
+console.log("===============================================");
+};
+menu();//invoke
+
+// * Ornek: Bilgi ver fonksiyonu
+//1.yontem
+const bilgiVer= (ad, soyad, dogum)=>{
+const bilgi =`Adim ${ad} Soyadim ${soyad} ve yasim ${2021-dogum}`
+return bilgi;
+};
+console.log(bilgiVer("Mucahit","Cakmak", 1992) );//invoke
+
+//2.yontem
+const bilgiVer1= (ad, soyad, dogum)=>`Adim ${ad} Soyadim ${soyad} 
+ve yasim ${2021-dogum}`;
+
+console.log(bilgiVer1("Mucahit","Yildiz", 1992) );//invoke
+
+// *ORNEK: SILINDIRIN HACMINI HESAPLAYAN BIR FONKSIYON YAZINIZ :
+
+const r= prompt("Yaricapi giriniz");
+const h= prompt("Yukseklik giriniz");
+
+const hacimHesaplama =(r,h)=> Math.PI*r*r*h;
+
+// * en yakin sayiya yuvarlar
+console.log("Silindirin hacmi:"+Math.round(hacimHesaplama(r,h)));
+
+//* virgulden sonra kac basamak istersek --toFixed
+console.log("Silindirin hacmi:"+(hacimHesaplama(r,h).toFixed(2)));
+
+//*en yakin tabana yuvarlar -- Math.floor
+console.log("Silindirin hacmi:"+Math.floor(hacimHesaplama(r,h)));
