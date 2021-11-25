6562ee86ac4263458fd4b2b531907f1bb2165af2
mucahit2664 <mucahit2664@gmail.com>
2021-11-25 21:00:04 +0300

    dom

--- bolum4_dom/ornek3_domManipule/app.js
+++ bolum4_dom/ornek3_domManipule/app.js
@@ -15,6 +15,24 @@ document.querySelector(".resim").addEventListener("mouseover", () => {
 document.querySelector(".resim").addEventListener("mouseout", () => {
   document.querySelector(".resim").src = "./img/lamba_off.gif";
 });
-  
+//Klavyeden bir tusa basildiginda bu fonksiyon devreye girsin
+document.querySelector(".textbox").onkeyup = function () {
+  const checkbox=document.querySelector(".checkbox");
+  const textbox=document.querySelector(".textbox");
+  if (checkbox.checked==true) {
+textbox.value=textbox.value.toUpperCase();
+  }else{
+    textbox.value=textbox.value.toLowerCase();
+  }
+};
+//HTML`DE input-div`in altina yeni bir h1 ekleme
+const sonDiv =document.querySelector(".input-div");
+  const h1 =document.createElement("h1");
+  const yazi = document.createTextNode("Programlama Dilleri");
+  h1.appendChild(yazi);
+ // document.body.appendChild(h1); 1.yol
+sonDiv.after(h1);
+//sonDiv.before(h1);//2.yol
+
   
   
