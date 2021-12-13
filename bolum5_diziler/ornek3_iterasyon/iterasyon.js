0e7640553b14c9f7992d0dc3041d4926310e2623
mucahit2664 <mucahit2664@gmail.com>
2021-12-13 17:49:58 +0300

    iterasyon

--- bolum5_diziler/ornek3_iterasyon/iterasyon.js
+++ bolum5_diziler/ornek3_iterasyon/iterasyon.js
@@ -24,40 +24,64 @@ console.log(`Negatif koordinat sayisi: ${negatif} Pozitif kordinat sayisi ${pozi
 //  ana programa döndürecek fonksiyonu yazınız. Eğer o
 // türden bir hayvan yok ise bulunamadığını yazdırsın.
 //--------------------------------------------------------
-const hayvanlar =["deve","fil","monkey","mouse","cat","dog","bird"];
-const ara=prompt("Aramak istediginiz hayvan turunu yaziniz").toLowerCase();
-const bulDondur=function () {
-    let hayvanSayisi=0;
-    for(let i in hayvanlar) {
-        if(hayvanlar[i]==ara){
-            hayvanSayisi++;
-        }
+// const hayvanlar =["deve","fil","monkey","mouse","cat","dog","bird"];
+// const ara=prompt("Aramak istediginiz hayvan turunu yaziniz").toLowerCase();
+// const bulDondur=function () {
+//     let hayvanSayisi=0;
+//     for(let i in hayvanlar) {
+//         if(hayvanlar[i]==ara){
+//             hayvanSayisi++;
+//         }
 
-    }
-    return hayvanSayisi;
-};
+//     }
+//     return hayvanSayisi;
+// };
 //bulDondur(ara);
 //bulDondur(ara);ve altada koymamiz gerekir
 
 
-const bul=bulDondur(ara);
-bul ==0 
-?console.log(`Aradiginiz hayvan turu bulunamadi`)
-:console.log(`Aradiginiz ${ara} ' den ${bul} adet vardir`) ;
+// const bul=bulDondur(ara);
+// bul ==0 
+// ?console.log(`Aradiginiz hayvan turu bulunamadi`)
+// :console.log(`Aradiginiz ${ara} ' den ${bul} adet vardir`) ;
+
+
 // ======================================================
 //                   FOR OF DÖNGÜSÜ
 // ======================================================
-// for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir. 
-//Bu dongude dizi icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
-
-
-
+// for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir.
+// Bu dongude dizi icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+const arabalar = ["Bmw", "Ford","Seat"];
+let yazi = "";  // Degisken olusturdum ve ilk deger olarak string dondurecegim icin hic birsey manasina ("")  yazdim. Eger toplama olsaydi sifir(0), carpma olsaydi bir(1) degerini verecektim.
+for(let x of arabalar){
+console.log(x);
+yazi += x + " ";
+}
+console.log(yazi);
 
 
 // ======================================================
 //                   FOREACH METODU
 // ======================================================
 //-------------- ÖRNEK -------------------
+let ogrenciler =["John","Ali","Can"];
+
+function yaz(a) {
+    console.log(a);
+}
+
+ogrenciler.forEach(yaz);
+
+ogrenciler.forEach((z) => console.log(z));
+console.log(ogrenciler);
+
+//!--------------ORNEK -------------------
+const fiyatlar =[200,300,125,450,333,123];
+let toplam =0;
+fiyatlar.forEach((f) =>(toplam +=f));
+console.log(toplam);
+document.querySelector(".toplam").innerHTML = `${toplam} TL` ;
+ 
 
 
 
